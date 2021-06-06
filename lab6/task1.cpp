387ca383ca267e06d6929c61c3ade01411ae07e2
Alexander <Alexander@DESKTOP-7AQMSSC>
2021-06-06 14:15:14 +0200

    Add project files.

---
+++ lab6/task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "task1.h"
+
+int main()
+{
+    LinkedList l;
+    Node* n = new Node(7);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(5);
+    l.add(n, 0);
+    l.add(n2, 1); //works
+    l.remove(3); //works
+    l.replace(n, n3);
+    cout << l.search(n2) << endl; //works 
+    cout << l.nodeAt(1)->getData() << endl; //works
+    //l.displayForward(); //works
+    l.displayBackward();
+}
